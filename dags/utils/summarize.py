eef16de09dad01eaa995371bee9b0d95698b9b1e
CristianMascia <74973735+CristianMascia@users.noreply.github.com>
2024-11-14 19:06:12 +0100

    Update summarize.py

--- dags/utils/summarize.py
+++ dags/utils/summarize.py
@@ -25,7 +25,7 @@ class LLMSummarizer:
     def format_prompt(self, news: str, weather: str, traffic: str) -> str:
         prompt = dedent(f'''
         <|system|>
-        You are an helpful assistant.<|end|>
+        You are a helpful assistant.<|end|>
         <|user|>
         The following information describes conditions relevant to
         taxi journeys through a single day in Glasgow, Scotland.
